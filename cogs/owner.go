c82be75d020c4add878a158e619c2065ac810f7a
SharpBit <31069084+SharpBit@users.noreply.github.com>
2019-07-21 11:48:34 +0800

    how old are these changes lol

--- cogs/owner.go
+++ cogs/owner.go
@@ -6,15 +6,32 @@ import (
 
 func presence(ctx *Context) {
 	args := ctx.Args
+	usage := "Usage: `//play <activity> [url:stream] [message]`"
+	if len(args) == 0 {
+		ctx.Send(usage)
+		return
+	}
 
 	switch args[0] {
 	case "play":
+		if len(args) < 2 {
+			ctx.Send(usage)
+			return
+		}
 		ctx.Session.UpdateStatus(0, strings.Join(args[1:], " "))
 		ctx.Send("Set `play` status to `" + strings.Join(args[1:], " ") + "`")
 	case "listen":
+		if len(args) < 2 {
+			ctx.Send(usage)
+			return
+		}
 		ctx.Session.UpdateListeningStatus(strings.Join(args[1:], " "))
 		ctx.Send("Set `listen` status to `" + strings.Join(args[1:], " ") + "`")
 	case "stream":
+		if len(args) < 3 {
+			ctx.Send(usage)
+			return
+		}
 		CleanURL := strings.Replace(strings.Replace(args[1], "<", "", 1), ">", "", 1)
 		ctx.Session.UpdateStreamingStatus(0, strings.Join(args[2:], " "), CleanURL)
 		ctx.Send("Set `stream` status to `" + strings.Join(args[2:], " ") + "` at URL <" + CleanURL + ">")
---
+++ cogs/utility.go
@@ -0,0 +1,76 @@
+package cogs
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"../utils"
+)
+
+// Hastebin responses
+type Hastebin struct {
+	Key string `json:"string"`
+}
+
+func tinyurl(ctx *Context) {
+	if len(ctx.Args) == 0 {
+		ctx.Send("Please pass in a URL")
+		return
+	}
+	link := ctx.Args[0]
+	url := "http://tinyurl.com/api-create.php?url=" + link
+
+	resp, err := http.Get(url)
+	if err != nil {
+		ctx.Send("An error has occured.")
+		return
+	}
+
+	defer resp.Body.Close()
+	ShortenedURL, _ := ioutil.ReadAll(resp.Body)
+
+	ctx.Send("Here is your shortened URL: <" + string(ShortenedURL) + ">")
+
+}
+
+func hastebin(ctx *Context) {
+	if len(ctx.Args) == 0 {
+		ctx.Send("Please send some code")
+		return
+	}
+	code := utils.CleanupCode(strings.Join(ctx.Args, " "))
+	fmt.Println(code)
+
+	jsonData := map[string]string{"data": code}
+	jsonValue, _ := json.Marshal(jsonData)
+
+	resp, err := http.Post("https://hastebin.com/documents", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		ctx.Send("error making POST request")
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	data := &Hastebin{}
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		ctx.Send("error unparsing returned JSON")
+		return
+	}
+
+	ctx.Send("Hastebin-ified! Here is your code: https://hastebin.com/" + data.Key + ".go")
+
+}
+
+func init() {
+	cog := NewCog("Utility", "Useful commands to help you out", false)
+	cog.AddCommand("tinyurl", "Shorten a URL with the tinyurl API", []string{}, tinyurl)
+	// cog.AddCommand("hastebin", "Hastebin-ify your code!", []string{}, hastebin)
+	cog.Load()
+}
