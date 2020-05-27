657fbc262015a1fdf3d32daca2ba0b2ca725d690
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-05-27 11:08:20 -0400

    lyrics command

--- cogs/utility.go
+++ cogs/utility.go
@@ -6,15 +6,28 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/SharpBit/go-enigma/commands"
 	"github.com/SharpBit/go-enigma/utils"
 )
 
-// Hastebin responses
 type Hastebin struct {
 	Key string `json:"string"`
+}
+
+type GeniusAPI struct {
+	Response struct {
+		Hits []struct {
+			Result struct {
+				Title   string `json:"full_title"`
+				URL     string `json:"url"`
+				SongArt string `json:"song_art_image_thumbnail_url"`
+			} `json:"result"`
+		} `json:"hits"`
+	} `json:"response"`
 }
 
 func tinyurl(ctx *commands.Context, link string) (err error) {
@@ -29,6 +42,82 @@ func tinyurl(ctx *commands.Context, link string) (err error) {
 	ShortenedURL, _ := ioutil.ReadAll(resp.Body)
 
 	_, err = ctx.Send("Here is your shortened URL: <" + string(ShortenedURL) + ">")
+	return
+}
+
+func lyrics(ctx *commands.Context, query ...string) (err error) {
+	q := url.Values{}
+	q.Set("q", strings.Join(query, " "))
+	req, _ := http.NewRequest("GET", "https://api.genius.com/search?"+q.Encode(), nil)
+	req.Header.Set("Authorization", "Bearer "+utils.GetConfig("geniusapi"))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("Genius returned status code %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	data := &GeniusAPI{}
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		return
+	}
+
+	if len(data.Response.Hits) < 1 {
+		ctx.Send("No results found.")
+		return
+	}
+
+	song := data.Response.Hits[0].Result
+	LyricsResp, err := http.Get(song.URL)
+	if err != nil {
+		return
+	}
+	defer LyricsResp.Body.Close()
+	if LyricsResp.StatusCode != 200 {
+		return fmt.Errorf("Genius returned status code %d", LyricsResp.StatusCode)
+	}
+	doc, err := goquery.NewDocumentFromReader(LyricsResp.Body)
+	if err != nil {
+		return
+	}
+	lyrics := strings.TrimSpace(doc.Find("div.lyrics").First().Text())
+	lines := strings.Split(lyrics, "\n")
+
+	chars := 0
+	split := 0
+	for i, line := range lines {
+		chars += len(line) + 1
+		if chars > 2048 {
+			split = i - 1
+			break
+		}
+	}
+	if split == 0 {
+		split = len(lines)
+	}
+
+	em := commands.NewEmbed().
+		SetTitle(song.Title).
+		SetURL(song.URL).
+		SetDescription(strings.Join(lines[:split], "\n")).
+		SetThumbnail(song.SongArt).
+		SetColor(0x2ecc71)
+
+	if split != len(lines) {
+		em.SetFooter("View full lyrics by clicking the title of the embed.")
+	}
+
+	_, err = ctx.SendComplex("", em.MessageEmbed)
 	return
 }
 
@@ -62,5 +151,6 @@ func init() {
 	cog := commands.NewCog("Utility", "Useful commands to help you out")
 	cog.AddCommand("tinyurl", "Shorten a URL with the tinyurl API", "<link>", tinyurl)
 	// cog.AddCommand("hastebin", "Hastebin-ify your code!", "<code>", hastebin)
+	cog.AddCommand("lyrics", "Find the lyrics for a song", "<query>", lyrics)
 	cog.Load()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/SharpBit/go-enigma
 
 go 1.14
 
-require github.com/bwmarrin/discordgo v0.20.3
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/bwmarrin/discordgo v0.20.3
+)
